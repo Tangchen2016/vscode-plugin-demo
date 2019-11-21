b757dc0e695c2ddaea7fc6d185e1c6acb7ef4c6f
tangchen <1061332325@qq.com>
2019-11-21 22:16:26 +0800

    feat: showErrorMessage & show date

--- src/extension.ts
+++ src/extension.ts
@@ -1,7 +1,9 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
+import * as moment from 'moment';
 
+moment.locale('zh-cn');
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
@@ -17,7 +19,10 @@ export function activate(context: vscode.ExtensionContext) {
 		// The code you place here will be executed every time your command is executed
 
 		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello VS Code!');
+		const dateStr = moment().format('YYYY-MM-DD HH:mm:ss dddd');
+		console.log(dateStr);
+		
+		vscode.window.showErrorMessage(dateStr);
 	});
 
 	context.subscriptions.push(disposable);
